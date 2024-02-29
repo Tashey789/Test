191c3be4100f71c3fd57ce3700c98bf090b7180b
unknown <02230109.cst@rub.edu.bt>
2024-02-29 20:11:40 +0600

    adding python.py file

---
+++ python.py
@@ -0,0 +1,9 @@
+Name = input("Enter your name")
+num_1 = float(input("Enter a number:"))
+num_2 = float(input("Enter a number:"))
+product = num_1 * num_2
+print("the product is :", product)
+sum = num_1 + num_2 
+print("the sum is :", sum)
+
+

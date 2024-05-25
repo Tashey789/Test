73630191e64ffd2f42563fea75fd5fe0c57ac184
unknown <02230109.cst@rub.edu.bt>
2024-05-26 00:30:43 +0600

    class_activity.py

---
+++ class_activity.py
@@ -0,0 +1,10 @@
+name= input("enter your name:")
+print("hello" , name)
+num1= float(input("enter a number:"))
+num2= float (input("enter another number:"))
+x= num1 + num2
+print('the sum is' , x)
+y= num1 * num2
+print("the product is", y)
+z= num1/num2
+print("the quotient is", z)
